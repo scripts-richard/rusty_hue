0fde276e5e5a18bf6e8a7178fb023d603b41e7bd
Richard Mills <scripts-richard@gmail.com>
2018-06-09 14:33:37 -0600

    Added argument parsing and information printout.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-extern crate rusty_hue;
+#[macro_use]
+extern crate clap;
 
+extern crate rusty_hue;
 use rusty_hue::hue::Hue;
 
+
 fn main() {
+    let matches = clap_app!(RustyHue =>
+        (version: "0.1")
+        (author: "Richard Mills <scripts.richard@gmail.com>")
+        (about: "Control your Hue lights from the command line.")
+        (@subcommand info =>
+            (about: "Displays information about Hue lights.")
+            (version: "0.1")
+        )
+    ).get_matches();
+
     let mut hue = Hue::new().unwrap();
-    hue.toggle_lights().unwrap();
+
+    match matches.subcommand_name() {
+        Some("info") => hue.print_info(),
+        _ => hue.toggle_lights().unwrap()
+    }
 }
